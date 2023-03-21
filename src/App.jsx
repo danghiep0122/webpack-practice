dcbe9aeec58b24e487dc55e89bc2eabf9f875789
danghiep <danghiep0122@gmail.com>
2023-03-21 12:26:28 +0700

    Init new repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { hot } from "react-hot-loader/root";
+
+function App() {
+    return (
+        <main>
+            <h1>Hello world</h1>
+        </main>
+    );
+}
+
+export default hot(App);
